fa5de559c49e615bdbcda9e00cec3a10161a5897
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 15:18:01 -0300

    ex009 resolvido

--- 01-basic-syntax/hard/ex009.py
+++ 01-basic-syntax/hard/ex009.py
@@ -1,7 +1,26 @@
 """
-Exercício 013
+Exercício 009
 
 Peça ao usuário informações para montar um pequeno cartaz de evento: nome do evento, data, horário, local e nome do organizador. Depois, exiba essas informações em um formato visualmente organizado no terminal, como se fosse um anúncio.
 """
 
 # Seu código começa aqui
+
+import os
+os.system("cls")
+
+print("Sobre o Evento informe:")
+nome = input("Digite o Nome: ")
+data = input("Digite a Data: ")
+horario = input("Digite o Horário: ")
+local = input("Digite o Local: ")
+organizador = input("Digite o Nome do Organizador: ")
+
+print(f"""
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+[NOME]...........{nome}
+[DATA]...........{data}
+[HORÁRIO]........{horario}
+[LOCAL]..........{local}
+[ORGANIZADOR]....{organizador}
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=""")
