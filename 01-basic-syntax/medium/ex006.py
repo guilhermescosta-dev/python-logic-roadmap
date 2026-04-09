9268d98940a60a322562475df2c503c0ca728072
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 14:52:10 -0300

    ex006 resolvido

--- 01-basic-syntax/medium/ex006.py
+++ 01-basic-syntax/medium/ex006.py
@@ -5,3 +5,16 @@ Peça ao usuário seu nome, idade, cidade e profissão. Depois, exiba uma aprese
 """
 
 # Seu código começa aqui
+
+nome = input("Digite seu Nome: ")
+idade = int(input("Digite sua Idade: "))
+cidade = input("Digite a Cidade em que você mora: ")
+profissao = input("Digite sua profissão: ")
+
+print(f"""
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+[NOME]........{nome}
+[IDADE].......{idade}
+[CIDADE]......{cidade}
+[PROFISSÃO]...{profissao}
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=""")
