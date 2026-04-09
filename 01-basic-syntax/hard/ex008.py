a5be8c2613ec7617a8466cc2c85e971b51614b5a
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 15:08:28 -0300

    ex008 resolvido

--- 01-basic-syntax/hard/ex008.py
+++ 01-basic-syntax/hard/ex008.py
@@ -1,7 +1,22 @@
 """
-Exercício 012
+Exercício 008
 
 Peça ao usuário seu nome, horário em que costuma estudar, matéria favorita, tempo médio diário de estudo e principal meta atual. Depois, exiba essas informações em formato de perfil de rotina, com cada item bem identificado.
 """
 
 # Seu código começa aqui
+
+nome = input("Digite seu Nome: ")
+horario = input("Digite o horário/periodo que costuma estudar: ")
+materia = input("Digite sua matéria favorita: ")
+tmde = input("Digite seu Tempo Médio Diário de Estudo: ")
+meta = input("Digite sua Principal Meta Atual: ")
+
+print(f"""
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+[NOME]................{nome}
+[HORÁRIO].............{horario}
+[MATÉRIA].............{materia}
+[TEMPO DE ESTUDO].....{tmde}
+[META]................{meta}
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=""")
