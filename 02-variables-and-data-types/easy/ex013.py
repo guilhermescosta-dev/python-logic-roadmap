cd46441ec7489bc092dd01725d5838bf5c1d1258
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 15:39:06 -0300

    ex013 resolvido

--- 02-variables-and-data-types/easy/ex013.py
+++ 02-variables-and-data-types/easy/ex013.py
@@ -5,3 +5,8 @@ Peça o nome do usuário e exiba o nome em letras maiúsculas e também em letra
 """
 
 # Seu código começa aqui
+
+nome = str(input("Digite seu Nome: "))
+
+print(f"[MAIÚSCULA] > {nome.upper()}")
+print(f"[minúscula] > {nome.lower()}")
