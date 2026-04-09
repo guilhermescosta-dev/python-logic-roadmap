b435962f08e67968ba6a013b0b5aebf9e0b6487b
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 12:45:21 -0300

    ex002 resolvido

--- 01-basic-syntax/easy/ex002.py
+++ 01-basic-syntax/easy/ex002.py
@@ -5,3 +5,11 @@ Peça o nome e a idade do usuário e exiba uma frase simples apresentando essas 
 """
 
 # Seu código começa aqui
+
+import os
+os.system("cls")
+
+nome = input("Digite seu Nome: ")
+idade = int(input("Digite sua Idade: "))
+
+print(f"Olá {nome}, você tem {idade} anos!")
