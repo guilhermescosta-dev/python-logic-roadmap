54f8d7d2e39cd93bf1f86cdff212a959a35d8806
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 15:41:23 -0300

    ex014 resolvido

--- 02-variables-and-data-types/easy/ex014.py
+++ 02-variables-and-data-types/easy/ex014.py
@@ -5,3 +5,9 @@ Peça a largura e a altura de um retângulo e calcule sua área.
 """
 
 # Seu código começa aqui
+
+largura = float(input("Digite a Largura: "))
+altura = float(input("Digite a Altura: "))
+area = largura * altura
+
+print(f"A Área do retângulo é {area}")
