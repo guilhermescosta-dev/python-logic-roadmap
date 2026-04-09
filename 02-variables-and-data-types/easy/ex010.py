df7a365451ae1946bb784c2b29dc14a5f3068d70
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 15:22:26 -0300

    ex010 resolvido

--- 02-variables-and-data-types/easy/ex010.py
+++ 02-variables-and-data-types/easy/ex010.py
@@ -5,3 +5,11 @@ Peça dois números ao usuário e exiba a soma entre eles.
 """
 
 # Seu código começa aqui
+
+from os import system 
+system("cls")
+
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+print(f"{n1} + {n2} = {n1+n2}")
