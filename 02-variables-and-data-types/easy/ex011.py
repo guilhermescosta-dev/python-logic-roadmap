5814e0a9cdae892704ea707d8c121614d01fafa6
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 15:25:30 -0300

    ex011 resolvido

--- 02-variables-and-data-types/easy/ex011.py
+++ 02-variables-and-data-types/easy/ex011.py
@@ -5,3 +5,8 @@ Peça um número ao usuário e exiba o dobro e o triplo desse número.
 """
 
 # Seu código começa aqui
+
+n1  = int(input("Digite um número: "))
+
+print(f"[DOBRO] -> {n1*2}")
+print(f"[TRIPLO] -> {n1*3}")
