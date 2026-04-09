d8f04ba9613270f6f9c4a3263e5d7c9a7e2259f2
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 14:42:37 -0300

    ex004 resolvido

--- 01-basic-syntax/medium/ex004.py
+++ 01-basic-syntax/medium/ex004.py
@@ -5,3 +5,9 @@ Peça ao usuário três informações separadas: um nome, uma comida e uma cor. 
 """
 
 # Seu código começa aqui
+
+nome = input("Digite seu Nome: ")
+comida = input("Digite sua Comida Favorita: ")
+cor = input("Digite sua Cor Favorita: ")
+
+print(f"Então {nome} sua comida favorita é {comida} e sua cor favorita é {cor}")
