4f346c806ee7ac1ef6d10bfe2ad6fff010e73dc2
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 15:44:15 -0300

    ex015 resolvido

--- 02-variables-and-data-types/medium/ex015.py
+++ 02-variables-and-data-types/medium/ex015.py
@@ -5,3 +5,11 @@ Peça três números ao usuário e calcule a média aritmética entre eles.
 """
 
 # Seu código começa aqui
+
+n1 = int(input("Digite o [1] Número: "))
+n2 = int(input("Digite o [2] Número: "))
+n3 = int(input("Digite o [3] Número: "))
+
+media = (n1 + n2 + n3) / 3
+
+print(f"A média desses números é {media}")
