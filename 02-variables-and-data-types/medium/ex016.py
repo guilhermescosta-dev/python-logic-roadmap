e570b69a83851d90f159b0e4464c2e9f58a4b45d
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 15:48:49 -0300

    ex016 resolvido

--- 02-variables-and-data-types/medium/ex016.py
+++ 02-variables-and-data-types/medium/ex016.py
@@ -5,3 +5,10 @@ Peça o preço de um produto e um valor de desconto (em porcentagem). Calcule o 
 """
 
 # Seu código começa aqui
+
+preco = float(input("Digite o Preço do Produto: R$"))
+desconto = float(input("Digite o PERCENTUAL [%] de Desconto: "))
+
+novopreco = preco - (preco*desconto/100)
+
+print(f"Este Produto com o desconto ficou: R${novopreco:.2f}")
