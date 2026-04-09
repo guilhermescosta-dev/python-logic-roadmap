2f6dae92d493359fda49bf7925d2663038552f38
Guilherme Silva Costa <guiscosta08@gmail.com>
2026-04-09 12:48:22 -0300

    ex003 resolvido

--- 01-basic-syntax/easy/ex003.py
+++ 01-basic-syntax/easy/ex003.py
@@ -5,3 +5,8 @@ Peça ao usuário sua cidade e seu estado e exiba uma frase mostrando de onde el
 """
 
 # Seu código começa aqui
+
+cidade = input("Digite a CIDADE a qual você mora: ")
+estado = input("Digite o ESTADO a qual você mora: ")
+
+print(f"Então você mora na cidade {cidade} que está situada no estado {estado}")
